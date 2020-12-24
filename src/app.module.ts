e47baaafde096b6967eb5b19466d118e8f96f283
Abhiram R Pai <abhirampai1999@gmail.com>
2020-12-24 21:45:49 +0530

    initial step of connecting to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
-
+require('dotenv').config();
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url:
+      process.env.MONGOURL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true,
+      synchronize: true,
+      logging: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    TasksModule],
 
 })
 export class AppModule {}
