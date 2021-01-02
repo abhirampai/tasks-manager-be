1a1569883902f11bbf0326cb6a3319e58797cecc
Pai026 <abhirampai1999@gmail.com>
2021-01-02 20:48:23 +0530

    minor fixes

--- src/auth/getUser.decorator.ts
+++ src/auth/getUser.decorator.ts
@@ -3,5 +3,7 @@ import { User } from "./entity/User.entity";
 
 export const GetUser = createParamDecorator((data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
+    delete req.user.iat
+    delete req.user.exp
     return req.user;
 });
--- src/tasks/dto/getTasksFilter.dto.ts
+++ src/tasks/dto/getTasksFilter.dto.ts
@@ -3,11 +3,11 @@ import { IsIn, IsNotEmpty, IsOptional } from "class-validator";
 import { TaskStatus } from "../models/task-status.enum";
 
 export class getTaskFilter {
-    @ApiProperty({type:String,example:'string'})
+    @ApiProperty({type:String,example:'string',required:false})
     @IsOptional()
     @IsIn([TaskStatus.OPEN,TaskStatus.INPROGRESS,TaskStatus.DONE])
     status:TaskStatus
 
-    @ApiProperty({type:String,example:'string'})
+    @ApiProperty({type:String,example:'string',required:false})
     searchTerm:string
 }
